23f6b7e74c478f3a127528b5b3951612fadeff0f
SafwanAbdal03 <safwan.abdal02@gmail.com>
2024-07-11 21:39:38 +0800

    New

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8080;
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
