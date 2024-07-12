7427289251bf5ed36b1174d57eaa64487978a8f7
SafwanAbdal03 <safwan.abdal02@gmail.com>
2024-07-12 10:41:41 +0800

    New

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [combinedBase64, setCombinedBase64] = useState('');
+  const [img, setImg] = useState(null);
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -11,45 +11,27 @@ function App() {
       .then(response => {
         console.log(response.data);
         if (response.data.errno === 0) {
-          const datastreams = response.data.data.datastreams;
-          const base64Values = datastreams.map(stream => stream.datapoints[0].value);
-          const combinedBase64 = base64Values.join(''); // Concatenate the base64 values
-          setCombinedBase64(combinedBase64);
+          const combinedBase64 = response.data.data.datastreams.map(stream => stream.datapoints[0].value).join('');
+          setImg(combinedBase64);
         } else {
           setError('Failed to load data');
         }
       })
       .catch(error => {
         console.error(error);
-        setError(`Error fetching data: ${error.message}`);
+        setError(`Error fetching data: ${error.response ? error.response.data.message : error.message}`);
       });
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {error ? (
-          <p>{error}</p>
-        ) : (
-          <div>
-            <h3>Base64 Data:</h3>
-            <img src={`data:image/jpeg;base64,${combinedBase64}`} alt="Fetched from API" />
-          </div>
-        )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Base64 Data:</p>
+        {error ? <p>{error}</p> : <img src={`data:image/jpeg;base64,${img}`} alt="Fetched from API" />}
       </header>
     </div>
   );
 }
 
 export default App;
+
