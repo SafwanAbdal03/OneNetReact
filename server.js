009f5a22321adb3dec79da2f75d518d32aae80c3
SafwanAbdal03 <safwan.abdal02@gmail.com>
2024-07-12 11:54:16 +0800

    n

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 
