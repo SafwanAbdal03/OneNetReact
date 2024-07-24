c45e0620d70315c0acca639377f3cd67fc42f402
SafwanAbdal03 <safwan.abdal02@gmail.com>
2024-07-24 15:32:47 +0800

    n

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
-const multer = require('multer');
-const FormData = require('form-data');
 
 const app = express();
 const port = process.env.PORT || 443;
@@ -10,9 +8,6 @@ const port = process.env.PORT || 443;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 app.use(cors());
-app.use(express.json());
-
-const upload = multer();
 
 app.get('/api/data', async (req, res) => {
   const apiKey = req.query.api;
@@ -39,32 +34,9 @@ app.get('/api/data', async (req, res) => {
   }
 });
 
-app.post('/api/upscale', upload.single('file'), async (req, res) => {
-  try {
-    const formData = new FormData();
-    formData.append('file', req.file.buffer, req.file.originalname);
-    formData.append('scale', '2'); // Upscale by a factor of 2
-    formData.append('noise', '1'); // Add noise reduction if necessary
-
-    const response = await axios.post('https://waifu2x.booru.pics/', formData, {
-      headers: {
-        ...formData.getHeaders()
-      },
-      responseType: 'arraybuffer'
-    });
-
-    res.set('Content-Type', 'image/jpeg');
-    res.send(response.data);
-  } catch (error) {
-    console.error('Error upscaling image:', error);
-    res.status(500).send('Error upscaling image');
-  }
-});
-
 app.listen(port, () => {
   console.log(`Proxy server is running at http://localhost:${port}`);
 });
 
 module.exports = app;
-
 
