0abc89b5d154a151bc0eb59940a146ef741d7858
SafwanAbdal03 <safwan.abdal02@gmail.com>
2024-07-29 10:34:52 +0800

    n

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("Data fetched:", data); // Debugging log
         if (data.errno === 0) {
           const datastreams = data.data.datastreams;
-          const allowedIds = ['3200_0_5750', '3200_1_5750', '3200_2_5750', '3200_3_5750', '3200_4_5750', '3200_5_5750', '3200_6_5750'];
+          const allowedIds = ['3200_0_5750', '3200_1_5750', '3200_2_5750', '3200_3_5750', '3200_4_5750', '3200_5_5750', '3200_6_5750', '3200_7_5750', '3200_8_5750'];
 
           // Sort the datastreams to match the order of allowed IDs
           const sortedDatastreams = allowedIds.map(id =>
